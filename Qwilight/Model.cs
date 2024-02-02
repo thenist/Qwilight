4b4ba51f259d13fbc739164db62ae7208c30a921
Taehui <taehui@taehui.ddns.net>
2024-02-02 11:25:59 +0900

    2024-02-02 오전 11:25

--- Qwilight/Model.cs
+++ Qwilight/Model.cs
@@ -1,21 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.ComponentModel;
 
 namespace Qwilight
 {
     public class Model : ObservableObject
     {
         public virtual void NotifyModel() => OnPropertyChanged(string.Empty);
-
-        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
-        {
-            try
-            {
-                base.OnPropertyChanged(e);
-            }
-            catch (Win32Exception)
-            {
-            }
-        }
     }
 }
