ed9955dc200135f71ac22a1b7304c0916d11b034
Taehui <taehui@taehui.ddns.net>
2024-04-26 12:58:49 +0900

    2024-04-26 오후 12:58

--- CI.cmd
+++ CI.cmd
@@ -4,9 +4,9 @@ SET VS2022=%PROGRAMFILES%\Microsoft Visual Studio\2022\Community
 SET MSBUILD=%VS2022%\Msbuild\Current\Bin\MSBuild.exe
 
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=x64 Flint\Flint.vcxproj
-Robocopy x64\Release Qwilight\Assets\AMD64 Flint.exe
+Robocopy Flint\x64\Release Qwilight\Assets\AMD64 Flint.exe
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=ARM64 Flint\Flint.vcxproj
-Robocopy ARM64\Release Qwilight\Assets\ARM64 Flint.exe
+Robocopy Flint\ARM64\Release Qwilight\Assets\ARM64 Flint.exe
 
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=x64 Igniter\Igniter.csproj
 Robocopy Igniter\bin\x64\Release Qwilight\Assets\AMD64 Igniter.exe
@@ -14,12 +14,12 @@ Robocopy Igniter\bin\x64\Release Qwilight\Assets\AMD64 Igniter.exe
 Robocopy Igniter\bin\ARM64\Release Qwilight\Assets\ARM64 Igniter.exe
 
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=x64 NVIDIA\NVIDIA.vcxproj
-Robocopy x64\Release Qwilight\Assets\AMD64 NVIDIA.dll
+Robocopy NVIDIA\x64\Release Qwilight\Assets\AMD64 NVIDIA.dll
 
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=x64 Xwindow\Xwindow.vcxproj
-Robocopy x64\Release Qwilight\Assets\AMD64 Xwindow.exe
+Robocopy Xwindow\x64\Release Qwilight\Assets\AMD64 Xwindow.exe
 "%MSBUILD%" -t:Build -p:Configuration=Release,Platform=ARM64 Xwindow\Xwindow.vcxproj
-Robocopy ARM64\Release Qwilight\Assets\ARM64 Xwindow.exe
+Robocopy Xwindow\ARM64\Release Qwilight\Assets\ARM64 Xwindow.exe
 
 IF %PROCESSOR_ARCHITECTURE% == AMD64 (
 	dotnet build Language\Language.csproj -c Release -p:Platform=x64
